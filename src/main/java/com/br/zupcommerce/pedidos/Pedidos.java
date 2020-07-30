124c98f2fb1b08abe357f74c8c7781db7bdf680a
JanaineB <barcelosjanaine@gmail.com>
2020-07-30 14:32:30 -0300

    developing the basic to start adding tdd and business logic

--- src/main/java/com/br/zupcommerce/pedidos/Pedidos.java
+++ src/main/java/com/br/zupcommerce/pedidos/Pedidos.java
@@ -1,4 +1,25 @@
 package com.br.zupcommerce.pedidos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(schema = "ezup", name = "pedidos")
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pedidos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nomeCliente;
+    private  String telefone;
+    private float valorProduto;
+    private float valorDesconto;
+    private float valorTotal;
 }
---
+++ src/main/java/com/br/zupcommerce/pedidos/PedidosRepository.java
@@ -0,0 +1,8 @@
+package com.br.zupcommerce.pedidos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidosRepository extends JpaRepository<Pedidos,Long> {
+}
